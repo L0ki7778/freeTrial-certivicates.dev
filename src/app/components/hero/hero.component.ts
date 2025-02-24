8216f33120a0a3a599737670fd134e1dff8aa996
Rene Heller <128136936+L0ki7778@users.noreply.github.com>
2025-02-24 03:20:57 +0100

    added new componet to hero + routing to hero

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,20 +1,15 @@
 import { Component } from '@angular/core';
-import { FooterComponent } from '../../shared/footer/footer.component';
-import { HeaderComponent } from '../../shared/header/header.component';
-import { HeaderDirective } from '../../directives/header.directive';
 import { Person, PersonList } from '../../interfaces/person';
 import { PersonPrefixPipe } from '../../pipes/person-prefix.pipe';
 import { LetterAnimationDirective } from '../../directives/letter-animation.directive';
-import { OutputComponent } from '../output/output.component';
-import { HashtagSyntaxComponent } from '../hashtag-syntax/hashtag-syntax.component';
-import { SignalComponent } from '../signal/signal.component';
+import { NgToolsComponent } from '../ng-tools/ng-tools.component';
 
 @Component({
   selector: 'app-hero',
   imports: [
     LetterAnimationDirective,
     PersonPrefixPipe,
-    
+    NgToolsComponent
   ],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
