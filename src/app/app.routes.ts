0dddac5b9a8ebda03d9c887910a2344bcc6450ec
Rene Heller <128136936+L0ki7778@users.noreply.github.com>
2025-02-24 03:00:00 +0100

    added routing and style

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HeroComponent } from './components/hero/hero.component';
+import { SignalPresentationComponent } from './components/signal-presentation/signal-presentation.component';
 
 export const routes: Routes = [
     {path: '', component:HeroComponent},
+    {path:'signals', component:SignalPresentationComponent}
 ];
