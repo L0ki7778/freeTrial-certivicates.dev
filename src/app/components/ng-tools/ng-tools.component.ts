4d919a0f6722a27a4d5270393aeb50111246c248
Rene Heller <128136936+L0ki7778@users.noreply.github.com>
2025-02-24 03:19:49 +0100

    new componen for ng templates

---
+++ src/app/components/ng-tools/ng-tools.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-tools',
+  imports: [],
+  templateUrl: './ng-tools.component.html',
+  styleUrl: './ng-tools.component.scss'
+})
+export class NgToolsComponent {
+
+}
