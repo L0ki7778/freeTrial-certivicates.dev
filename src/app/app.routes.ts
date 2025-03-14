a3cc1a944b3b4d0a06dd43513821aefdf0914ca6
Rene-Heller <rene-heller@developerakademie.com>
2025-03-14 11:23:05 +0100

    added demo.component to header and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HeroComponent } from './components/hero/hero.component';
 import { SignalPresentationComponent } from './components/signal-presentation/signal-presentation.component';
+import { DemoComponent } from './components/demo/demo.component';
 
 export const routes: Routes = [
-    {path: '', component:HeroComponent},
-    {path:'signals', component:SignalPresentationComponent}
+  { path: '', component: HeroComponent },
+  { path: 'signals', component: SignalPresentationComponent },
+  { path: 'demo', component: DemoComponent },
 ];
