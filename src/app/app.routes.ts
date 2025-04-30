88a7b4577fe1d97046bc088068726dd1ba54c52a
Rene-Heller <rene-heller@developerakademie.com>
2025-04-30 16:44:46 +0200

    added new route and routerconfig for easier url-param subscription

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HeroComponent } from './components/hero/hero.component';
 import { SignalPresentationComponent } from './components/signal-presentation/signal-presentation.component';
 import { DirectivesPlaygroundComponent } from './components/directives-playground/directives-playground.component';
 import { DemoComponent } from './components/demo/demo.component';
+import { RouteInputsComponent } from './components/route-inputs/route-inputs.component';
 
 export const routes: Routes = [
   { path: '', component: HeroComponent },
   { path: 'signals', component: SignalPresentationComponent },
   { path: 'demo', component: DemoComponent },
-  { path: 'directive', component: DirectivesPlaygroundComponent }
+  { path: 'directive', component: DirectivesPlaygroundComponent },
+  { path: 'routing', component: RouteInputsComponent }
 ];
