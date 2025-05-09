5aaab24aad3700c99e55077e4cc373800219f043
Rene-Heller <rene-heller@developerakademie.com>
2025-05-09 08:22:58 +0200

    added pokemon mock data

---
+++ src/app/testing-practice/service/mockdata.ts
@@ -0,0 +1,38 @@
+export const pokeList = {
+  1: {
+    name: "bulbasaur",
+    url: "https://pokeapi.co/api/v2/pokemon/1/"
+  },
+  2: {
+    name: "ivysaur",
+    url: "https://pokeapi.co/api/v2/pokemon/2/"
+  },
+  3: {
+    name: "venusaur",
+    url: "https://pokeapi.co/api/v2/pokemon/3/"
+  },
+  4: {
+    name: "charmander",
+    url: "https://pokeapi.co/api/v2/pokemon/4/"
+  },
+  5: {
+    name: "charmeleon",
+    url: "https://pokeapi.co/api/v2/pokemon/5/"
+  },
+  6: {
+    name: "charizard",
+    url: "https://pokeapi.co/api/v2/pokemon/6/"
+  },
+  7: {
+    name: "squirtle",
+    url: "https://pokeapi.co/api/v2/pokemon/7/"
+  },
+  8: {
+    name: "wartortle",
+    url: "https://pokeapi.co/api/v2/pokemon/8/"
+  },
+  9: {
+    name: "blastoise",
+    url: "https://pokeapi.co/api/v2/pokemon/9/"
+  }
+}
