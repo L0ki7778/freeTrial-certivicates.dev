6b9b7c81f9f7ff786e6f67c5814076169c52c092
Rene-Heller <rene-heller@developerakademie.com>
2025-05-09 08:23:49 +0200

    added container-component to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { SignalPresentationComponent } from './components/signal-presentation/si
 import { DirectivesPlaygroundComponent } from './components/directives-playground/directives-playground.component';
 import { DemoComponent } from './components/demo/demo.component';
 import { RouteInputsComponent } from './components/route-inputs/route-inputs.component';
+import { ContainerComponentComponent } from './testing-practice/container-component/container-component.component';
 
 export const routes: Routes = [
   { path: '', component: HeroComponent },
   { path: 'signals', component: SignalPresentationComponent },
   { path: 'demo', component: DemoComponent },
   { path: 'directive', component: DirectivesPlaygroundComponent },
-  { path: 'routing', component: RouteInputsComponent }
+  { path: 'routing', component: RouteInputsComponent },
+  { path: 'testing', component: ContainerComponentComponent }
 ];
