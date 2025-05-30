fc1c7bf25db43f2838cbe3911508322a93268c6b
Rene-Heller <rene-heller@developerakademie.com>
2025-05-30 08:33:58 +0200

    added httpclient to container.spec

--- src/app/testing-practice/container-component/container-component.component.spec.ts
+++ src/app/testing-practice/container-component/container-component.component.spec.ts
@@ -1,23 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContainerComponentComponent } from './container-component.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { TestDemoService } from '../service/test-demo.service';
 
-xdescribe('ContainerComponentComponent', () => {
+fdescribe('ContainerComponentComponent', () => {
   let component: ContainerComponentComponent;
   let fixture: ComponentFixture<ContainerComponentComponent>;
+  let service: any;
 
   beforeEach(async () => {
+    const pokemonService = jasmine.createSpyObj('TestDemoService', ['loadPokemon']);
+
     await TestBed.configureTestingModule({
-      imports: [ContainerComponentComponent]
+      imports: [ContainerComponentComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        { provide: TestDemoService, useValue: pokemonService }
+      ]
     })
       .compileComponents();
 
     fixture = TestBed.createComponent(ContainerComponentComponent);
+    service = TestBed.inject(TestDemoService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
+
+
     expect(component).toBeTruthy();
   });
 });
